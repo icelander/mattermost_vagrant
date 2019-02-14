c6191f6aa14c2c959a19deacc3f7b742672c4ada
Paul Rothrock <paul@movetoiceland.com>
2019-02-14 15:03:09 -0500

    Working cluster! Setting up SMB share.
    Signed-off-by: Paul Rothrock <paul@movetoiceland.com>

--- setup.sh
+++ setup.sh
@@ -4,14 +4,14 @@ apt-get -q -y update
 
 # Sets the root password for MariaDB
 export DEBIAN_FRONTEND=noninteractive
-debconf-set-selections <<< 'mariadb-server-10.0 mysql-server/root_password password root'
-debconf-set-selections <<< 'mariadb-server-10.0 mysql-server/root_password_again password root'
-apt-get -q -y install mariadb-server
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+apt-get -q -y install mysql-server
 
 apt-get -y -q install nginx
 
 # Allows cluster to connect
-sed -i 's|127.0.0.1|0.0.0.0|g' /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i 's|bind-address|#bind-address|g' /etc/mysql/mysql.conf.d/mysqld.cnf
 
 # Copy Nginx configuration
 mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.orig
