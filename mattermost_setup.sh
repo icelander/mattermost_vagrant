186b87712c51a309142b996cf6a9c835d61df4cf
Paul Rothrock <paul@movetoiceland.com>
2019-07-11 10:56:09 -0400

    Added IP address configuration
    Signed-off-by: Paul Rothrock <paul@movetoiceland.com>

--- mattermost_setup.sh
+++ mattermost_setup.sh
@@ -10,11 +10,11 @@ mount -a
 # cat /vagrant/hosts >> /etc/hosts
 
 # Download Mattermost
-if [ ! -f /vagrant/mattermost-5.12.1-linux.amd64.tar.gz ]; then
-    wget --quiet https://releases.mattermost.com/5.12.1/mattermost-5.12.1-linux-amd64.tar.gz
-    cp mattermost-5.12.1-linux-amd64.tar.gz /vagrant/mattermost-5.12.1-linux.amd64.tar.gz
+if [ ! -f /vagrant/mattermost-5.12.0-linux.amd64.tar.gz ]; then
+    wget --quiet https://releases.mattermost.com/5.12.0/mattermost-5.12.0-linux-amd64.tar.gz
+    cp mattermost-5.12.0-linux-amd64.tar.gz /vagrant/mattermost-5.12.0-linux.amd64.tar.gz
 else
-	cp /vagrant/mattermost-5.12.1-linux.amd64.tar.gz ./	
+	cp /vagrant/mattermost-5.12.0-linux.amd64.tar.gz ./	
 fi
 
 tar -xzf mattermost*.gz
@@ -27,6 +27,9 @@ ln -s /vagrant/license.txt /opt/mattermost/license.txt
 mv /opt/mattermost/config/config.json /opt/mattermost/config/config.orig.json
 
 jq -s '.[0] * .[1]' /opt/mattermost/config/config.orig.json /vagrant/config.json > /vagrant/config.vagrant.json
+
+sed -i .bak 's/"OverrideHostname": ""/"OverrideHostname": "#IP_ADDRESS"/' /opt/mattermost/config/config.json
+
 chmod 777 /vagrant/config.vagrant.json
 
 cp /vagrant/config.vagrant.json /opt/mattermost/config/config.json
